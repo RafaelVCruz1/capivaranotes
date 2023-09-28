4d4dda8422f475bf4d929afa8afe70f1381415ee
RafaelVCruz1 <rafael.cruz3@estudante.ifms.edu.br>
2023-09-28 11:23:11 -0400

    Alterações de boas práticas

--- js/db.js
+++ js/db.js
@@ -1,137 +1,157 @@
 import { openDB } from "idb";
 
-let db;
-async function criarDB(){
+class CapivaraNotes {
+  constructor() {
+    this.db = null;
+    this.init();
+  }
+
+  async init() {
     try {
-        db = await openDB('banco', 1, {
-            upgrade(db, oldVersion, newVersion, transaction){
-                switch  (oldVersion) {
-                    case 0:
-                    case 1:
-                        const store = db.createObjectStore('anotacao', {
-                            keyPath: 'titulo'
-                        });
-                        store.createIndex('id', 'id');
-                        console.log("banco de dados criado!");
-                }
-            }
-        });
-        console.log("banco de dados aberto!");
-    }catch (e) {
-        console.log('Erro ao criar/abrir banco: ' + e.message);
-    }
-}
-
-window.addEventListener('DOMContentLoaded', async event => {
-    await criarDB();
-    document.getElementById('btnCadastro').addEventListener('click', adicionarAnotacao);
-    document.getElementById('btnCarregar').addEventListener('click', buscarTodasAnotacoes);
-
-});
-
-
-
-async function buscarTodasAnotacoes(){
-    if(db == undefined){
-        console.log("O banco de dados está fechado.");
-    }
-    const tx = await db.transaction('anotacao', 'readonly');
-    const store = await tx.objectStore('anotacao');
-    const anotacoes = await store.getAll();
-    if(anotacoes){
-        const divLista = anotacoes.map(anotacao => {
-            return `<div class="item">
-                    <p>Anotação ⬇</p>
-                    <p>Título: ${anotacao.titulo} - ${anotacao.data} </p>
-                    <p>Categoria: ${anotacao.categoria}</p>
-                    <p>Descrição: ${anotacao.descricao}</p>
-                    <button class="btnExcluir">Excluir</button>
-                    <button class="btnAlterar" titulo="${anotacao.titulo}">Alterar</button>
-                   </div>`;
-        });
-        listagem(divLista.join(' '));
-        const excluirAnotacao = document.querySelectorAll('.btnExcluir') 
-        excluirAnotacao.forEach((excluirAnotacao, index) => {
-            excluirAnotacao.addEventListener('click', () => excluirAnot(anotacoes[index].titulo))
-        });
-
-        const alterarAnotacao = document.querySelectorAll('.btnAlterar') 
-        alterarAnotacao.forEach(altera => {
-            altera.addEventListener("click", (event) => {
-            const titulo = event.target.getAttribute("titulo");
-            alteracao(titulo, anotacoes);
-        });
-    });
-        };
-
-      
+      this.db = await openDB('banco', 1, {
+        upgrade(db, oldVersion, newVersion, transaction) {
+          switch (oldVersion) {
+            case 0:
+            case 1:
+              const store = db.createObjectStore('anotacao', {
+                keyPath: 'titulo'
+              });
+              store.createIndex('id', 'id');
+              console.log("Banco de dados criado!");
+          }
+        }
+      });
+      console.log("Banco de dados aberto!");
+    } catch (e) {
+      console.log('Erro ao criar/abrir banco: ' + e.message);
     }
 
+    this.addEventListeners();
+  }
 
-async function excluirAnot(titulo) {
-    const tx = await db.transaction('anotacao', 'readwrite');
-    const store = await tx.objectStore('anotacao');
-    try {
-        await store.delete(titulo);
-        buscarTodasAnotacoes()
-        console.log('Anotação deletada com sucesso!');
-    } catch (error) {
-        console.error('Erro ao deletar anotação:', error);
-        tx.abort();
+  addEventListeners() {
+    document.getElementById('btnCadastro').addEventListener('click', () => this.adicionarAnotacao());
+    document.getElementById('btnCarregar').addEventListener('click', () => this.buscarTodasAnotacoes());
+  }
+
+  async buscarTodasAnotacoes() {
+    if (!this.db) {
+      console.log("O banco de dados está fechado.");
+      return;
     }
-}
 
-async function alterarAnot(titulo){
-    const tx = await db.transaction('anotacao', 'readwrite');
-    const store = await tx.objectStore('anotacao');
-    const altanotacao = await store.get(titulo)
-    const Alteracategoria = document.getElementById("Alteracategoria").value
-    const Alteradescricao = document.getElementById("Alteradescricao").value
-    const Alteradata = document.getElementById("Alteradata").value
-
-    altanotacao.categoria =  Alteracategoria
-    altanotacao.data = Alteradata
-    altanotacao.descricao = Alteradescricao
-
-    await store.put(altanotacao)
-    await tx.done
-}
-
-function alteracao(titulo, anotacoes) {
-    const anotacaoAltera = anotacoes.find(param => param.titulo === titulo)
-    let div = document.createElement('div')
-    div.innerHTML = `
-    
-    <input type="text" id="Alteracategoria" placeholder="Categoria" value="${anotacaoAltera.categoria}">
-    <textarea id="Alteradescricao" cols="30" rows="10" placeholder="Descrição" value="${anotacaoAltera.descricao}"></textarea>
-    <input type="date" id="Alteradata" value="${anotacaoAltera.data}"/>
-    <button id="btnAlteracao">Alterar</button>
-    `
-    const btndaAlteracao = div.querySelector('#btnAlteracao')
-    btndaAlteracao.addEventListener('click', () => alterarAnot(titulo))
-    document.getElementById('resultados').appendChild(div)
-}
-
-async function adicionarAnotacao() {
-    let titulo = document.getElementById("titulo").value;
-    let categoria = document.getElementById("categoria").value;
-    let descricao = document.getElementById("descricao").value;
-    let data = document.getElementById("data").value;
-    const tx = await db.transaction('anotacao', 'readwrite')
+    const tx = this.db.transaction('anotacao', 'readonly');
     const store = tx.objectStore('anotacao');
-    try {
-        await store.add({ titulo: titulo, categoria: categoria, descricao: descricao, data: data });
-        await tx.done;
-        apagarAnot();
-        console.log('Registro adicionado com sucesso!');
-    } catch (error) {
-        console.error('Erro ao adicionar registro:', error);
-        tx.abort();
+    const anotacoes = await store.getAll();
+
+    if (anotacoes) {
+      const divLista = anotacoes.map(anotacao => this.createAnotacaoHTML(anotacao));
+      this.listagem(divLista.join(' '));
+      this.addExcluirEventListeners(anotacoes);
+      this.addAlterarEventListeners(anotacoes);
     }
-}
+  }
 
-function listagem(text){
+  createAnotacaoHTML(anotacao) {
+    return `
+      <div class="item">
+        <p>Anotação ⬇</p>
+        <p>Título: ${anotacao.titulo} - ${anotacao.data} </p>
+        <p>Categoria: ${anotacao.categoria}</p>
+        <p>Descrição: ${anotacao.descricao}</p>
+        <button class="btnExcluir">Excluir</button>
+        <button class="btnAlterar" titulo="${anotacao.titulo}">Alterar</button>
+      </div>`;
+  }
+
+  addExcluirEventListeners(anotacoes) {
+    const excluirAnotacao = document.querySelectorAll('.btnExcluir');
+    excluirAnotacao.forEach((excluirAnotacao, index) => {
+      excluirAnotacao.addEventListener('click', () => this.excluirAnot(anotacoes[index].titulo));
+    });
+  }
+
+  addAlterarEventListeners(anotacoes) {
+    const alterarAnotacao = document.querySelectorAll('.btnAlterar');
+    alterarAnotacao.forEach(altera => {
+      altera.addEventListener("click", (event) => {
+        const titulo = event.target.getAttribute("titulo");
+        this.alteracao(titulo, anotacoes);
+      });
+    });
+  }
+
+  async excluirAnot(titulo) {
+    const tx = this.db.transaction('anotacao', 'readwrite');
+    const store = tx.objectStore('anotacao');
+
+    try {
+      await store.delete(titulo);
+      this.buscarTodasAnotacoes();
+      console.log('Anotação deletada com sucesso!');
+    } catch (error) {
+      console.error('Erro ao deletar anotação:', error);
+      tx.abort();
+    }
+  }
+
+  async alterarAnot(titulo, categoria, descricao, data) {
+    const tx = this.db.transaction('anotacao', 'readwrite');
+    const store = tx.objectStore('anotacao');
+    const altanotacao = await store.get(titulo);
+
+    altanotacao.categoria = categoria;
+    altanotacao.data = data;
+    altanotacao.descricao = descricao;
+
+    await store.put(altanotacao);
+    await tx.done;
+  }
+
+  alteracao(titulo, anotacoes) {
+    const anotacaoAltera = anotacoes.find(param => param.titulo === titulo);
+    let div = document.createElement('div');
+    div.innerHTML = `
+      <input type="text" id="Alteracategoria" placeholder="Categoria" value="${anotacaoAltera.categoria}">
+      <textarea id="Alteradescricao" cols="30" rows="10" placeholder="Descrição">${anotacaoAltera.descricao}</textarea>
+      <input type="date" id="Alteradata" value="${anotacaoAltera.data}" />
+      <button id="btnAlteracao">Alterar</button>
+    `;
+    const btndaAlteracao = div.querySelector('#btnAlteracao');
+    btndaAlteracao.addEventListener('click', () => {
+      const categoria = document.getElementById("Alteracategoria").value;
+      const descricao = document.getElementById("Alteradescricao").value;
+      const data = document.getElementById("Alteradata").value;
+      this.alterarAnot(titulo, categoria, descricao, data);
+      this.buscarTodasAnotacoes();
+    });
+    document.getElementById('resultados').appendChild(div);
+  }
+
+  async adicionarAnotacao() {
+    const titulo = document.getElementById("titulo").value;
+    const categoria = document.getElementById("categoria").value;
+    const descricao = document.getElementById("descricao").value;
+    const data = document.getElementById("data").value;
+
+    const tx = this.db.transaction('anotacao', 'readwrite');
+    const store = tx.objectStore('anotacao');
+
+    try {
+      await store.add({ titulo, categoria, descricao, data });
+      await tx.done;
+      this.apagarAnot();
+      console.log('Registro adicionado com sucesso!');
+    } catch (error) {
+      console.error('Erro ao adicionar registro:', error);
+      tx.abort();
+    }
+  }
+
+  listagem(text) {
     document.getElementById('resultados').innerHTML = text;
+  }
+
 }
 
-
+window.addEventListener('DOMContentLoaded', () => new CapivaraNotes());
